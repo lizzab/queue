c86a9b1cc6e1f5dcc4c222fe851da622c8c85fe4
lizzab <Jjibber12>
2020-03-04 19:54:30 -0500

    Commit

---
+++ Queue.py
@@ -0,0 +1,9 @@
+from LinkedList import SingleLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.mylist = SingleLinkedList
+
+    def push_queue(self, data):
+        SingleLinkedList.add_tail()
